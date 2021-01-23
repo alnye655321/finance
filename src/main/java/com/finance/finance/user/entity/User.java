b5b33122f3d59e2b46be779e20dd890a8eee6a98
alnye655321 <wiggles@nyedigital.com>
2021-01-23 07:41:08 -0500

    created basic user api structure

---
+++ src/main/java/com/finance/finance/user/entity/User.java
@@ -0,0 +1,76 @@
+package com.finance.finance.user.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    private long id;
+    private String name;
+    private String hashedPassword;
+
+
+
+    private String email;
+
+    public User() {
+
+    }
+
+    public User(String name, String hashedPassword, String email) {
+        this.name = name;
+        this.hashedPassword = hashedPassword;
+        this.email = email;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Column(name = "name", nullable = false)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(name = "hashedPassword", nullable = false)
+    public String getHashedPassword() {
+        return hashedPassword;
+    }
+
+    public void setHashedPassword(String hashedPassword) {
+        this.hashedPassword = hashedPassword;
+    }
+
+    @Column(name = "email", nullable = false)
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", name=" + name + ", hashPassword=" + hashedPassword + ", email=" + email
+                + "]";
+    }
+
+}
